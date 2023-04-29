517f91b54ed604a114a736f45640e0e02a8a3c3d
jumelleCL <132051029+jumelleCL@users.noreply.github.com>
2023-04-29 22:53:55 +0200

    Add files via upload

---
+++ ProyectosPersonales/NodeJS/bot/config.js
@@ -0,0 +1,7 @@
+let config = {
+    token: 'MTA3MDc2MzAzNzA3OTkxNjYwNA.Gbi-ml.pNI9YnGqZ5nCDlo5tRG40mU1t8I-ozIhqnZRS0',
+    prefix: 'Juancho,',
+    "ownerIDS":["893553232956260352"]
+}
+
+module.exports = config;
