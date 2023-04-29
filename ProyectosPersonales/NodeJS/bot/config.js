e87860230fd7641b1febdd0c43395164fbe7f352
jumelleCL <132051029+jumelleCL@users.noreply.github.com>
2023-04-29 22:55:00 +0200

    Delete ProyectosPersonales/NodeJS/bot directory

--- ProyectosPersonales/NodeJS/bot/config.js
+++
@@ -1,7 +0,0 @@
-let config = {
-    token: 'MTA3MDc2MzAzNzA3OTkxNjYwNA.Gbi-ml.pNI9YnGqZ5nCDlo5tRG40mU1t8I-ozIhqnZRS0',
-    prefix: 'Juancho,',
-    "ownerIDS":["893553232956260352"]
-}
-
-module.exports = config;
